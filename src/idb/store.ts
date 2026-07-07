3ac6d822a0eaec90944358c1b54d959ca6e42282
YG. Song <insaint03@gmail.com>
2026-07-07 10:01:12 +0900

    update: store.indexName getter setup

--- src/idb/store.ts
+++ src/idb/store.ts
@@ -4,10 +4,16 @@ import { Queriable } from "./common";
 /** IDBObjectStore wrapper  */
 export default class StoreProxy extends Queriable<IDBObjectStore> {
   protected _tx:IDBTransaction;
+  protected _index:{[name:string]:IndexProxy};
 
   constructor(tx:IDBTransaction, name:string) {
     super(tx.objectStore(name));
     this._tx = tx;
+    this._index = {};
+    // getter
+    this.indexNames.forEach((index)=>{
+      Object.defineProperty(this, index, {get:()=>this.index(index)})
+    });
   }
 
   public get store():IDBObjectStore { 
@@ -19,7 +25,7 @@ export default class StoreProxy extends Queriable<IDBObjectStore> {
   // - IDBObjectStore.name
 
   // read props
-  public get indexNames() { return this.store.indexNames; }
+  public get indexNames():string[] { return Array.from(this.store.indexNames); }
   public get transaction() { return this.store.transaction; }
   public get autoIncrement() { return this.store.autoIncrement; }
 
@@ -61,7 +67,24 @@ export default class StoreProxy extends Queriable<IDBObjectStore> {
   // @ProxiedIndex
   // IDBObjectStore.index
   public index(name:string):IndexProxy {
-    return new IndexProxy(this.store, name);
+    if(this._index[name] == undefined) {
+      this._index[name] = new IndexProxy(this.store, name);
+    }
+    return this._index[name];
+  }
+
+  /**
+   * retrieve cursor from query statement
+   * 
+
+   *  
+   *  
+   *  
+   * @param stmt 
+   * @param index 
+   */
+  public async *query(stmt:string, index?:string):AsyncGenerator {
+
   }
 
 }
